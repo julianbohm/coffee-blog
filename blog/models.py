0d8a801261958cf708f5a8e368ee55da6b54dfda
Julian Bohm <julianbohm9@gmail.com>
2024-12-21 14:37:52 +0000

    add authentication and crud funtionality

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,10 @@ class CoffeePost(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField()
+    
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     date_posted = models.DateTimeField(auto_now_add=True)
     average_rating = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import CoffeePost
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-def hello_coffee(request):
-    return HttpResponse("have a Joe!")
+@login_required
+def create_post(request):
+    if request.method == "POST":
+        title = request.POST['title']
+        description = request.POST['description']
+        CoffeePost.objects.create(title=title, description=description, author=request.user)
+        return redirect('home')
+        return render(request, 'create_post.html')
