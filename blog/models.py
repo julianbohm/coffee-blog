11f62643a2b57ea01169591fa56fdc1118bee1ca
Julian Bohm <julianbohm9@gmail.com>
2024-12-21 15:31:25 +0000

    add rating model

--- blog/models.py
+++ blog/models.py
@@ -7,10 +7,17 @@ class CoffeePost(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField()
-    
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     date_posted = models.DateTimeField(auto_now_add=True)
     average_rating = models.FloatField(default=0.0)
 
     def __str__(self):
         return self.title
+
+class Rating(models.Model):
+    post = models.ForeignKey(CoffeePost, related_name='ratings', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stars = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
+
+    def __str__(self):
+        return f'{self.user} - {self.post} - {self.stars}'
