f58172207b55bc58bb1eec7f6f41851c5aafe7c4
Julian Bohm <julianbohm9@gmail.com>
2025-01-15 13:29:12 +0000

    login as main page, html unify style

--- coffee_lovers_blog/urls.py
+++ coffee_lovers_blog/urls.py
@@ -14,14 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
-
+from django.contrib import admin  
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  
-    path("", include("welcome.urls")),  
-    path('blog/', include('blog.urls')),  
+    path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/accounts/login/', permanent=True)), 
+    path('blog/', include('blog.urls')),
     path('summernote/', include('django_summernote.urls')),
-    path('accounts/', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),  
 ]
