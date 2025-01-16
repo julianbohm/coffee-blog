cb5c9bfbd361d31b8e60982a150aadddbf7df1aa
Julian Bohm <julianbohm9@gmail.com>
2025-01-16 12:22:00 +0000

    fix authentication urls

--- coffee_lovers_blog/urls.py
+++ coffee_lovers_blog/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin  
 from django.views.generic import RedirectView
 
@@ -24,4 +26,4 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('summernote/', include('django_summernote.urls')),
     path('accounts/', include('allauth.urls')),  
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
